0e8d960e5164fcadfab52b5de489d6967b38d1b6
crazynam321 <crazynam321@yahoo.com>
2015-01-16 15:56:36 -0800

    Added functionality to deal with activity labels, summarising and averaging

--- UCI HAR Dataset/run_analysis.R
+++ UCI HAR Dataset/run_analysis.R
@@ -8,6 +8,9 @@
 #  and stored in a second dataset containing the average and the activity names.
 #  This dataset is than written to 'averages.csv'. 
 #
+#	I am aware it is generally bad practice, but I chose to omit any error 
+#  checking at this point for the lack of available time.  
+#
 #  run()		:  Main function.  Call directly to run program.
 #
 #  get_merge()		:  Merges the files 'X_test.txt' and 'X_train.txt' 
@@ -16,12 +19,16 @@
 #  name_cols(df)	:  Extracts 'mean' and 'std' variables from the dataset
 #			   subsetting the original and applying descriptive names.
 #			   Takes a dataset as a parameter.
+#
+#  label_activities(df)	:  Appropriately replaces numeric value in Activity with
+#			   It's proper string value.
+#			   Takes a dataset as a parameter.
 ################################################################################
 
 run <- function()
 {
 	
-	library(dplyr)	     # For tbl_df()
+	library(dplyr)	     # For tbl_df(), ddply()
 	library(data.table)  # for fread(), rbindlist()
 
 	# get merged data set
@@ -30,17 +37,19 @@ run <- function()
 	# Select columns with only 'mean' and 'row' and label appropriately
 	combined <- name_cols(combined) 
 
-	combined
+	# Label all activities appropriately
+	combined <- label_activities(combined)
+
 	# Fix minor error with double 'Body' in column names
 	names(combined) <- sub("BodyBody", "Body", names(combined))	
 
-	# Calculate averages on all columns except 'Activity'
-	combined$Averages <- rowMeans(combined[2:67], na.rm=T)
+	# Crunch dataset down to unique 'Subject' and calculate means
+	combined <- ddply(combined, .(Subject,Activity), numcolwise(mean))
 
-	# Calculate averages on all rows(except 'Activity')
-	combined <- rbind(combined, c("Averages", colMeans(combined[,c(2:68)], na.rm=T)))
+	# Calculate averages on all rows(except 'Subject' & 'Activity')
+	# and store in new column 'Averages'
+	combined <- cbind(combined, "Averages" = rowMeans(combined[3:68], na.rm=T))
 
-	combined[do.call(order,combined),]	
 	# Write file out to 'averages.csv'
 	write.table(combined, file="./averages.csv", sep=",", row.names=FALSE)
 }
@@ -50,13 +59,14 @@ get_merge <- function()
 {
 	# load 'X_train.txt': Training set.
 	train_set <- fread("./train/X_train.txt")
-	train_activity <- fread("./train/y_train.txt")
+	train_set[,"Activity"] <- fread("./train/y_train.txt")
+	train_set[,"Subject"] <- fread("./train/subject_train.txt")
+
 	# load 'X_test.txt'  : Test set.
 	test_set <- fread("./test/X_test.txt")
-	test_activity <- fread("./test/y_test.txt")
+	test_set[,"Activity"] <- fread("./test/y_test.txt")
+	test_set[,"Subject"]  <- fread("./test/subject_test.txt")
 
-	train_set <- cbind(train_set, "Activity" = train_activity)
-	test_set <- cbind(test_set, "Activity" = test_activity)
 
 	# merge both files together
 	combined <- tbl_df(rbindlist(list(train_set,test_set)))
@@ -78,11 +88,10 @@ name_cols <- function(combined = NULL)
 
 	## Format 'patt' vars into regEx for exact match of 'V(valid_names$V1)'
         patt <- sprintf('^V%i$', valid_names$V1)
-	valid_names <- rbind(valid_names, data.frame(V1=562,V2="Activity"))
+	valid_names <- rbind(valid_names, data.frame(V1=c(562,563),V2=c("Activity", "Subject")))
 
 	## Create a single string of arguments separated by OR - '|"
-	patt <- paste(as.character(patt), collapse="|")
-	patt <- paste("Activity", patt, sep = "|")
+	patt <- paste(as.character(c("Subject", "Activity", patt)), collapse="|")
 
 	# Match 'patt' with the column names of 'combined' and subset 'combined'
 	# appropriately on the right columns
@@ -92,7 +101,29 @@ name_cols <- function(combined = NULL)
 	# Change Column names to the ones provided in 'features.txt'
 	names(combined) <- valid_names$V2
 	
-	# Right now combined$Activity is the last column so this reorders it 
-	# so now it becomes the first column
-	combined[,c(67,1:66)]
+	#  Order columns so 'Subject', 'Activity' and then the rest
+	combined[,c(68,67,1:66)]
+}
+
+# Function to match 'Activity' numbers to their proper string (e.g. - '1' to 'WALKING')
+label_activities <- function(combined = NULL)
+{
+	activities <- read.table("./activity_labels.txt")
+	
+	# tried a few ways but was unable to figure this part out from lectures so 
+	# resorted to a for loop so it is functional albeit probably slower.
+	for(i in 1:nrow(combined))
+	{
+		# Basic test for all possible cases. 
+		switch(combined$Activity[i],
+			"1" = (combined$Activity[i] = as.character(activities$V2[1])),
+			"2" = (combined$Activity[i] = as.character(activities$V2[2])),
+			"3" = (combined$Activity[i] = as.character(activities$V2[3])),
+			"4" = (combined$Activity[i] = as.character(activities$V2[4])),
+			"5" = (combined$Activity[i] = as.character(activities$V2[5])),
+			"6" = (combined$Activity[i] = as.character(activities$V2[6]))
+			)  # End switch
+	}
+
+	combined
 }
