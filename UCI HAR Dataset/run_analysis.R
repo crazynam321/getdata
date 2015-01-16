c41229aff8ab350f5173b4deafef7ff667b2ba06
crazynam321 <crazynam321@yahoo.com>
2015-01-16 11:30:07 -0800

    Re-wrote to account for "activities.txt". Doh!

--- UCI HAR Dataset/run_analysis.R
+++ UCI HAR Dataset/run_analysis.R
@@ -16,9 +16,6 @@
 #  name_cols(df)	:  Extracts 'mean' and 'std' variables from the dataset
 #			   subsetting the original and applying descriptive names.
 #			   Takes a dataset as a parameter.
-#
-#  write_file(df)	:  Writes file to disk.
-#			   Takes a dataset as a parameter.
 ################################################################################
 
 run <- function()
@@ -33,21 +30,19 @@ run <- function()
 	# Select columns with only 'mean' and 'row' and label appropriately
 	combined <- name_cols(combined) 
 
+	combined
 	# Fix minor error with double 'Body' in column names
 	names(combined) <- sub("BodyBody", "Body", names(combined))	
 
-	# Calculate averages on all columns
-	averages <- cbind(Averages = colMeans(combined, na.rm=T), 
-			  "Activity Names"=names(combined)) 
+	# Calculate averages on all columns except 'Activity'
+	combined$Averages <- rowMeans(combined[2:67], na.rm=T)
 
+	# Calculate averages on all rows(except 'Activity')
+	combined <- rbind(combined, c("Averages", colMeans(combined[,c(2:68)], na.rm=T)))
+
+	combined[do.call(order,combined),]	
 	# Write file out to 'averages.csv'
-	write_file(averages)
-}
-
-# Save to file 'averages.csv'		
-write_file <- function(averages = NULL)
-{
-	write.table(averages, file="./averages.csv", sep=",", row.names=FALSE)
+	write.table(combined, file="./averages.csv", sep=",", row.names=FALSE)
 }
 
 # function to load two data sets and return the merged version
@@ -55,9 +50,14 @@ get_merge <- function()
 {
 	# load 'X_train.txt': Training set.
 	train_set <- fread("./train/X_train.txt")
+	train_activity <- fread("./train/y_train.txt")
 	# load 'X_test.txt'  : Test set.
 	test_set <- fread("./test/X_test.txt")
-	
+	test_activity <- fread("./test/y_test.txt")
+
+	train_set <- cbind(train_set, "Activity" = train_activity)
+	test_set <- cbind(test_set, "Activity" = test_activity)
+
 	# merge both files together
 	combined <- tbl_df(rbindlist(list(train_set,test_set)))
 
@@ -75,12 +75,14 @@ name_cols <- function(combined = NULL)
 	# (Based off of given criteria it seemed to me meanFreq() is neither of the two so 
 	# I omitted that from my subset. e.g. '\\(')
    	valid_names <- subset(features, grepl(paste(c("mean\\(","std\\("), collapse="|"), V2))	
-	
+
 	## Format 'patt' vars into regEx for exact match of 'V(valid_names$V1)'
         patt <- sprintf('^V%i$', valid_names$V1)
+	valid_names <- rbind(valid_names, data.frame(V1=562,V2="Activity"))
 
 	## Create a single string of arguments separated by OR - '|"
 	patt <- paste(as.character(patt), collapse="|")
+	patt <- paste("Activity", patt, sep = "|")
 
 	# Match 'patt' with the column names of 'combined' and subset 'combined'
 	# appropriately on the right columns
@@ -90,5 +92,7 @@ name_cols <- function(combined = NULL)
 	# Change Column names to the ones provided in 'features.txt'
 	names(combined) <- valid_names$V2
 	
-	combined	
+	# Right now combined$Activity is the last column so this reorders it 
+	# so now it becomes the first column
+	combined[,c(67,1:66)]
 }
